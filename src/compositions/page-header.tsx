57acccd72ec4401e77705c1de006f8ee5fe49f0c
webdev123123 <passionate.it.developer@gmail.com>
2021-03-11 14:54:59 +0000

    fix: Add Favicons and Stuff

--- src/compositions/page-header.tsx
+++ src/compositions/page-header.tsx
@@ -27,6 +27,11 @@ export const PageHeader: FC<Props> = ({ markdownTitle, description, children, aw
       <Head>
         <title>{title}</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
         {description && <meta name="description" content={description} />}
         <meta property="og:type" content="website" />
         <meta property="og:url" content={pageUrl} />
