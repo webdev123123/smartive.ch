3fb2f44e4abb467b10f1d97be10fdd8be77ff4bd
webdev123123 <passionate.it.developer@gmail.com>
2021-03-22 13:44:39 +0000

    feat(SMRTVCH-23): Add Plausible

--- src/components/customers-list.tsx
+++ src/components/customers-list.tsx
@@ -1,3 +1,4 @@
+import { usePlausible } from 'next-plausible';
 import Image from 'next/image';
 import { FC } from 'react';
 import { Customer } from '../data/customers';
@@ -6,10 +7,29 @@ type Props = {
   customers: Customer[];
 };
 
-export const CustomersList: FC<Props> = ({ customers }) => (
-  <div className="grid grid-cols-3 lg:grid-cols-6 gap-12 lg:gap-24">
-    {customers.map(({ name, logo }) => (
-      <Image key={name} src={logo} alt={name} height="60" width="170" objectFit="contain" />
-    ))}
-  </div>
-);
+export const CustomersList: FC<Props> = ({ customers }) => {
+  const plausible = usePlausible();
+
+  return (
+    <div className="grid grid-cols-3 lg:grid-cols-6 gap-12 lg:gap-24">
+      {customers.map(({ name, logo }) => (
+        <Image
+          onClick={() => {
+            plausible('Customer Click', {
+              props: {
+                name,
+                url: window?.location,
+              },
+            });
+          }}
+          key={name}
+          src={logo}
+          alt={name}
+          height="60"
+          width="170"
+          objectFit="contain"
+        />
+      ))}
+    </div>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, LazyMotion, m as motion } from 'framer-motion';
+import PlausibleProvider from 'next-plausible';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
@@ -9,31 +10,37 @@ import '../styles/globals.css';
 
 const loadFramerMotionFeatures = () => import('../utils/framer-motion-features').then((res) => res.default);
 
+const PLAUSIBLE_DOMAIN = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+const PLAUSIBLE_ENABLED = process.env.NEXT_PUBLIC_PLAUSIBLE_ENABLED === 'true';
+
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
+
   return (
-    <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, viewport-fit=cover"
-        />
-      </Head>
-      <LazyMotion strict features={loadFramerMotionFeatures}>
-        <Navigation />
-        <AnimatePresence>
-          <motion.div
-            className="lg:container lg:mx-auto px-4 pt-8"
-            key={pathname}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Component {...pageProps} />
-          </motion.div>
-        </AnimatePresence>
-        <Footer />
-      </LazyMotion>
-    </div>
+    <PlausibleProvider domain={PLAUSIBLE_DOMAIN} enabled={PLAUSIBLE_ENABLED}>
+      <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, viewport-fit=cover"
+          />
+        </Head>
+        <LazyMotion strict features={loadFramerMotionFeatures}>
+          <Navigation />
+          <AnimatePresence>
+            <motion.div
+              className="lg:container lg:mx-auto px-4 pt-8"
+              key={pathname}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <Component {...pageProps} />
+            </motion.div>
+          </AnimatePresence>
+          <Footer />
+        </LazyMotion>
+      </div>
+    </PlausibleProvider>
   );
 }
