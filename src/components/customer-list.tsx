9dcdc30df32d4097be163366934f468df74e8d90
webdev123123 <passionate.it.developer@gmail.com>
2021-03-31 11:38:58 +0200

    fix: Plausible Click Tracking

--- src/components/customer-list.tsx
+++ src/components/customer-list.tsx
@@ -2,13 +2,14 @@ import { usePlausible } from 'next-plausible';
 import Image from 'next/image';
 import { FC } from 'react';
 import { Customer } from '../data/customers';
+import { PlausibleEvents } from '../utils/tracking';
 
 type Props = {
   customers: Customer[];
 };
 
 export const CustomerList: FC<Props> = ({ customers }) => {
-  const plausible = usePlausible();
+  const plausible = usePlausible<PlausibleEvents>();
 
   return (
     <div className="grid grid-cols-3 lg:grid-cols-6 gap-12 lg:gap-24">
@@ -18,7 +19,7 @@ export const CustomerList: FC<Props> = ({ customers }) => {
             plausible('Customer Click', {
               props: {
                 name,
-                url: window?.location,
+                url: window?.location.toString(),
               },
             });
           }}
---
+++ src/utils/tracking.ts
@@ -0,0 +1,9 @@
+export type PlausibleEvents = {
+  'Customer Click': {
+    name: string;
+    url: string;
+  };
+  404: {
+    path: string;
+  };
+};
