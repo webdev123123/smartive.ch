7f50f7efe2c2cff252bee26922de441ab6c38aff
webdev123123 <passionate.it.developer@gmail.com>
2021-09-02 12:57:21 +0000

    chore: Switch to guetzli navigation component

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -1,14 +1,48 @@
 import { AnimatePresence, m as motion } from 'framer-motion';
 import { useRouter } from 'next/dist/client/router';
 import React, { FC } from 'react';
-import { Navigation } from '../components/navigation';
+import { Logo, Navigation } from '@smartive/guetzli';
+import { usePlausible } from 'next-plausible';
+import { PlausibleEvents } from '../utils/tracking';
+import NextLink from 'next/link';
+
+const Main = [
+  { label: 'Angebot', link: '/angebot' },
+  { label: 'Projekte', link: '/projekte' },
+  { label: 'Team', link: '/team' },
+  { label: 'Agentur', link: '/agentur' },
+  { label: 'Blog', link: '/blog' },
+  { label: 'Jobs', link: '/jobs' },
+];
+
+const Meta = [
+  { label: '+41 44 552 55 99', link: 'tel:0041445525599' },
+  { label: 'hello@smartive.ch', link: 'mailto:hello@smartive.ch' },
+];
 
 export const Page: FC = ({ children }) => {
   const { pathname } = useRouter();
+  const plausible = usePlausible<PlausibleEvents>();
 
   return (
     <div>
-      <Navigation />
+      <Navigation
+        mainLinks={Main}
+        metaLinks={Meta}
+        currentPathname={pathname}
+        home={<Logo className="h-[21px] w-auto py-[4px]" />}
+        linkWrapper={NextLink}
+        onMetaLinkClick={(value: string) =>
+          plausible('Contact Click', {
+            props: {
+              value,
+              url: window?.location.toString(),
+              component: 'navigation',
+              device: 'desktop',
+            },
+          })
+        }
+      />
       <AnimatePresence>
         <motion.div
           className="lg:container lg:mx-auto px-4 pt-8 max-w-[100vw]"
