343df911473128209601931f1f229ff9092f9ca6
webdev123123 <passionate.it.developer@gmail.com>
2021-10-05 10:11:39 +0200

    fix: Mobile navigation

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -1,9 +1,10 @@
-import { useRouter } from 'next/dist/client/router';
-import React, { FC } from 'react';
 import { Logo, Navigation } from '@smartive/guetzli';
+import { domAnimation, LazyMotion } from 'framer-motion';
 import { usePlausible } from 'next-plausible';
-import { PlausibleEvents } from '../utils/tracking';
+import { useRouter } from 'next/dist/client/router';
 import NextLink from 'next/link';
+import React, { FC } from 'react';
+import { PlausibleEvents } from '../utils/tracking';
 
 const Main = [
   { label: 'Angebot', link: '/angebot' },
@@ -25,24 +26,26 @@ export const Page: FC = ({ children }) => {
 
   return (
     <div>
-      <Navigation
-        mainLinks={Main}
-        metaLinks={Meta}
-        currentPathname={pathname}
-        home={<Logo className="h-[21px] w-auto py-[4px]" />}
-        linkWrapper={(props) => <NextLink prefetch={false} {...props} />}
-        onMetaLinkClick={(value: string) =>
-          plausible('Contact Click', {
-            props: {
-              value,
-              url: window?.location.toString(),
-              component: 'navigation',
-              device: 'desktop',
-            },
-          })
-        }
-        onHomeLinkContextMenu={() => (window.location.href = '/brand')}
-      />
+      <LazyMotion strict features={domAnimation}>
+        <Navigation
+          mainLinks={Main}
+          metaLinks={Meta}
+          currentPathname={pathname}
+          home={<Logo className="h-[21px] w-auto py-[4px]" />}
+          linkWrapper={(props) => <NextLink prefetch={false} {...props} />}
+          onMetaLinkClick={(value: string) =>
+            plausible('Contact Click', {
+              props: {
+                value,
+                url: window?.location.toString(),
+                component: 'navigation',
+                device: 'desktop',
+              },
+            })
+          }
+          onHomeLinkContextMenu={() => (window.location.href = '/brand')}
+        />
+      </LazyMotion>
       <div id="pageContent" className="lg:container lg:mx-auto px-4 pt-8 max-w-[100vw]">
         {children}
       </div>
