c6898901c76cf5d98bb5c8b0cb6fd8662202cab3
webdev123123 <passionate.it.developer@gmail.com>
2021-11-01 08:52:16 +0000

    chore(deps): update nextjs monorepo to v12 (major)

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
+  swcMinify: true,
   eslint: {
     // Don't run linting during build since on CI we manually run the lint command
     ignoreDuringBuilds: true,
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1536], // 1536px is our max container size
   },
 });
