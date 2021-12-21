b32c92e50f7fe1ade890c158b3a1af94bae47917
webdev123123 <passionate.it.developer@gmail.com>
2021-12-21 07:40:16 +0000

    fix: Text adjustments for flutter

--- src/pages/was-ist/flutter.tsx
+++ src/pages/was-ist/flutter.tsx
@@ -11,8 +11,8 @@ const PWA = () => (
       description="Flutter"
     >
       <Copy>
-        Eigentlich bevorzugen wir ja <Link href="/was-ist/pwa/">PWA&apos;s</Link>. Wenn du aber unbedingt eine klassische App
-        im Appstore haben musst, hilft uns <strong>Flutter</strong>, deine App nur einmal zu programmieren und trotzdem für{' '}
+        Eigentlich bevorzugen wir ja <Link href="/was-ist/pwa">PWAs</Link>. Wenn du aber unbedingt eine klassische App im
+        Appstore haben musst, hilft uns <strong>Flutter</strong>, deine App nur einmal zu programmieren und trotzdem für{' '}
         <strong>Android</strong>, <strong>iOS</strong>, <strong>Windows</strong> und <strong>macOS</strong> in die jeweiligen
         Appstores auszuliefern.
       </Copy>
@@ -37,18 +37,13 @@ const PWA = () => (
           Damit wir uns das sparen können, gibt es verschiedene Technologien, die Doppelspurigkeiten unnötig machen.{' '}
           <Link href="https://flutter.dev">Flutter</Link> ist eine davon. Andere sind{' '}
           <Link href="https://dotnet.microsoft.com/apps/xamarin">Xamarin</Link>,{' '}
-          <Link href="https://reactnative.dev/">React Native</Link>
-          oder <Link href="https://ionicframework.com/">Ionic</Link>. Wir haben mindestens Füsse, meistens Beine und einmal
-          sogar so ziemlich alles reingehalten (der Kopf war immer dabei). Überzeugt hat uns bisher nur Flutter.
-        </Copy>
-        <Copy>
-          Ein grosser Vorteil von Flutter: die Apps laufen{' '}
-          <Explainer title="direkt auf dem Betriebssystem laufende Apps">nativ</Explainer> auf der jeweiligen Plattform. Das
-          macht sie schneller und flüssiger in der Bedienbarkeit. Gegensatz den aufgezählten Konkurrenten.
+          <Link href="https://reactnative.dev">React Native</Link> oder <Link href="https://ionicframework.com">Ionic</Link>.
+          Wir haben mindestens Füsse, meistens Beine und einmal sogar so ziemlich alles reingehalten (der Kopf war immer
+          dabei). Überzeugt hat uns bisher nur Flutter.
         </Copy>
         <Copy>
           Die Auslieferung in die Appstores ist aber leider immer noch mit mehr Arbeit verbunden. Deshalb, Hand aufs Herz:
-          Ist eine <Link href="/was-ist/pwa/">PWA</Link> nicht doch die passendere Lösung?
+          Ist eine <Link href="/was-ist/pwa">PWA</Link> nicht doch die passendere Lösung?
         </Copy>
       </PageSection>
     </main>
