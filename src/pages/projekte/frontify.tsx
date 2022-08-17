aa5d42ea33ba231cb4c74daeeed1f4e7f9860d2a
webdev123123 <passionate.it.developer@gmail.com>
2022-08-17 14:24:04 +0000

    feat: update frontify

--- src/pages/projekte/frontify.tsx
+++ src/pages/projekte/frontify.tsx
@@ -11,6 +11,9 @@ import { NextImageCard } from '../../components/image-card';
 import { getRandomTeasers } from '../../utils/teasers';
 import { Employee, getEmployeeByName } from '../../data/employees';
 import { Contact } from '../../components/contact';
+import { Testimonial } from '../../components/testimonial';
+import { Quote } from '../../data/quotes';
+import Quotes from '../../data/quotes.json';
 
 const STATIC_IMAGES = {
   flyer: '/images/projekte/frontify/210908_FRO2101_3766.jpg',
@@ -27,9 +30,10 @@ type Props = {
   images: typeof STATIC_IMAGES;
   teasers: Teaser[];
   contact: Employee;
+  quote: Quote;
 };
 
-const Frontify: NextPage<Props> = ({ images, teasers, contact }) => {
+const Frontify: NextPage<Props> = ({ images, teasers, contact, quote }) => {
   return (
     <Page>
       <PageHeader
@@ -95,11 +99,15 @@ const Frontify: NextPage<Props> = ({ images, teasers, contact }) => {
                 Schritt zu halten. Das Produkt will gebaut, betrieben und gewartet werden.{' '}
               </TextBlock>
               <TextBlock title="">
-                Wir haben kein Produkt im Rücken, sondern diverse Projekte. Und ganz viel Know-how über die neusten
-                Technologien und Trends. Wir machen Hackdays und Code-Retreats, erzählen auf Konferenzen, organisieren Labs,
-                halten Workshops und: alle <Explainer title={'Nicht die Schokolinse von Nestlé'}>Smarties</Explainer> haben
-                ein umfangreiches Weiterbildungsbudget. Damit wir zum Beispiel genau wissen, was der Unterschied zwischen{' '}
-                <Link href="https://blog.smartive.ch/how-should-we-react-to-stencil-and-svelte-ca76d631cf95?source=rss----2be72cec0825---4">
+                Wir haben kein Produkt im Rücken, sondern <Link href="https://smartive.ch/projekte">diverse Projekte</Link>.
+                Und ganz viel Know-how über die neusten Technologien und Trends. Wir machen Hackdays und Code-Retreats,
+                erzählen auf Konferenzen, organisieren Labs, halten Workshops und: alle{' '}
+                <Explainer title={'Nicht die Schokolinse von Nestlé'}>Smarties</Explainer> haben ein umfangreiches
+                Weiterbildungsbudget. Damit wir zum Beispiel genau wissen, was der Unterschied zwischen{' '}
+                <Link
+                  newTab
+                  href="https://blog.smartive.ch/how-should-we-react-to-stencil-and-svelte-ca76d631cf95?source=rss----2be72cec0825---4"
+                >
                   Stencil und Svelte
                 </Link>{' '}
                 ist und was es mit dem nächsten Hype auf sich hat.
@@ -107,8 +115,8 @@ const Frontify: NextPage<Props> = ({ images, teasers, contact }) => {
             </div>
             <div>
               <TextBlock title="Das erste Date">
-                Angefangen hat unsere Zusammenarbeit mit Reviews und Workshops zu Elasticsearch und der Architektur des
-                Frontify-Styleguides.
+                Angefangen hat unsere Zusammenarbeit mit <Link href="/angebot/solution-review">Reviews</Link> und Workshops
+                zu Elasticseach und der Architektur des Frontify-Styleguides.
               </TextBlock>
               <TextBlock title="">
                 Als dann der Umbau von Terrific zu <Link href="/was-ist/react">React</Link> auf dem Plan stand, haben wir
@@ -157,19 +165,25 @@ const Frontify: NextPage<Props> = ({ images, teasers, contact }) => {
               Bei einem Innovation-Day haben wir Ideen für ein paar neue Blocks ausprobiert. Da dürfen wir noch nicht zu viel
               verraten, aber eine der Ideen scheint es in die offizielle Produkt-Roadmap geschafft zu haben. Die vorhandenen
               Blocks reichen nicht? Genau darum haben wir die Plattform soweit aufgebohrt, dass du jetzt{' '}
-              <Link href="https://www.frontify.com/en/blog/the-new-era-of-brand-guidelines/">deinen eigenen Blocks</Link>{' '}
+              <Link newTab href="https://www.frontify.com/en/blog/the-new-era-of-brand-guidelines/">
+                deine eigenen Blocks
+              </Link>{' '}
               beisteuern kannst. Nun sind der Styleguide-Kreativität keine Grenzen mehr gesetzt.
             </TextBlock>
             <Screenshot image={{ url: images.blockMaps, originalHeight: 1966, originalWidth: 3584 }} />
             <Screenshot image={{ url: images.blockCharts, originalHeight: 1966, originalWidth: 3584 }} />
           </Grid>
+          <Testimonial background="cornflower" quote={quote} />
         </Section>
         <Section title="Alles besser">
           <TextBlock title="Bessere Accessibility">
             Über 15% aller Anwender*innen sind in irgendeiner Form beeinträchtigt. Tendenz steigend. Accessibility wird also
             immer wichtiger, ist aber meist ziemlich umständlich umzusetzen und kostet viel Zeit. Mit React-Aria kriegen wir
             mit wenig Aufwand eine gute Zugänglichkeit hin. Sogar bei den{' '}
-            <Link href="https://smartive.ch/blog/accessible-dropdown-using-react-aria">anspruchsvollen Dropdowns</Link>.
+            <Link newTab href="https://smartive.ch/blog/accessible-dropdown-using-react-aria">
+              anspruchsvollen Dropdowns
+            </Link>
+            .
           </TextBlock>
           <Grid cols={2}>
             <div>
@@ -181,9 +195,12 @@ const Frontify: NextPage<Props> = ({ images, teasers, contact }) => {
               </TextBlock>
               <TextBlock title="">
                 Unsere Design-System Geheimnisse behalten wir noch ein bisschen für uns, einen Einsteiger-Tipp gibts aber:
-                Nutz einfach die Defaults von <Link href="/was-ist/react#tailwind">Tailwind</Link> und hör auf, Custom Values
-                für einzelne Elemente zu definieren. Dann hast du schon mal sehr viel System in deinem Design. Und genau
-                darum gehts bei einem Design-System. 😉
+                Nutz einfach mal die Defaults von{' '}
+                <Link newTab href="https://smartive-website-p9p1l1gpb-smartive.vercel.app/blog/tailwind-gives-superpowers">
+                  Tailwind
+                </Link>{' '}
+                und hör auf, Custom Values für einzelne Elemente zu definieren. Dann hast du schon mal sehr viel System in
+                deinem Design. Und genau darum gehts bei einem Design-System. 😉
               </TextBlock>
             </div>
             <div>
@@ -194,7 +211,7 @@ const Frontify: NextPage<Props> = ({ images, teasers, contact }) => {
                 froh um Hundertmeter statt Ausdauerlauf.
               </TextBlock>
               <TextBlock title="">
-                Deshalb haben wir mit dem Developer-SDK für Frontify ein CLI-Tool entwickelt, mit dem einzelne Content-Blocks
+                Deshalb haben wir mit dem Developer-SDK mit Frontify ein CLI-Tool entwickelt, mit dem einzelne Content-Blocks
                 isoliert gestartet werden können. Natürlich einfach erweiterbar, damit wir oft benötigte Tasks in Zukunft
                 einfach mit rein packen können.
               </TextBlock>
@@ -237,6 +254,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     props: {
       images,
       teasers,
+      quote: Quotes['marc-frontify'],
       contact,
     },
   };
